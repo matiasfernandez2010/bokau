c0ae41399090f4fc2654f7d600cb1c2bae2e252c
Matias Fernandez <matiasfernandez2010@gmail.com>
2014-07-15 18:27:32 -0400

    Improved user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 
 	validates :name, presence: true, length: { maximum: 25 }
 
 	VALID_PHONE_REGEX = /\A(\(\d{3}\)|\d{3})-?.?\d{3}-?.?\d{4}+\z/i
 	validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }, uniqueness: { case_sensitive: false }
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+(?<!\.)\.(?!\.)[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
 	has_secure_password
